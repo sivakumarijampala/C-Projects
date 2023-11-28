47398bc6c9133e3ff6a0649e4e14f5512dac05c3
sivakumari2000 <sivakumari.jampala@corenuts.com>
2023-11-28 16:32:33 +0530

    c

---
+++ AccessSpecifiers2/Program4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessSpecifiers2
+{
+    //case4:consuming members of class from  child class in different project
+     class Program4:AccessSpecifiers.Program
+    {
+        static void Main(string[] args)
+        {
+            Program4 p = new Program4();
+            p.Test3();
+            p.Test4();
+            p.Test5();
+
+        }
+    }
+}
---
+++ AccessSpecifiers2/Program5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessSpecifiers2
+{
+    //case5:consuming members of a class from non-child class of different project
+    //private:not possible,protected:not possible,internal:not possible,
+    //protectedinternal:not possible
+    class Program5
+    {
+        static void Main()
+        {
+            AccessSpecifiers.Program ap = new AccessSpecifiers.Program();
+            ap.Test5();
+        }
+    }
+}
