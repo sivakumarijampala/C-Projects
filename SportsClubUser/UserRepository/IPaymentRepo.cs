f66a9a632d6daee50a058f88c17cbc7a9ff1a84a
sivakumari2000 <sivakumari.jampala@corenuts.com>
2023-11-28 16:52:55 +0530

    first commit

---
+++ SportsClubUser/UserRepository/IPaymentRepo.cs
@@ -0,0 +1,9 @@
+﻿using Models;
+
+namespace SportClubProject.UserRepository
+{
+    public interface IPaymentRepo
+    {
+        public Payment SavePaymentDetails(Payment payment);
+    }
+}
---
+++ SportsClubUser/UserRepository/PaymentRepoImpl.cs
@@ -0,0 +1,42 @@
+﻿using Models;
+using SportClubProject.Repository;
+using SportClubProject.Services;
+
+namespace SportClubProject.UserRepository
+{
+    public class PaymentRepoImpl:IPaymentRepo
+    {
+        //sports dbcontext
+        public readonly SportsDbContext sportsDbContext;
+
+        //logger
+        public readonly ILogger<PaymentRepoImpl> logger;
+
+        //user service
+         public readonly UserService userService;
+        public PaymentRepoImpl(SportsDbContext sportsDbContext, UserService userService, ILogger<PaymentRepoImpl> logger)
+        {
+            this.sportsDbContext = sportsDbContext;
+            this.userService = userService;
+            this.logger = logger;
+        }
+
+
+        //saving payment details
+        public Payment SavePaymentDetails(Payment payment)
+        {
+            try
+            {
+                logger.LogInformation("entered into save payment details in payment repo impl");
+                sportsDbContext.Add(payment);
+                sportsDbContext.SaveChanges();
+                return payment;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "error occur while saving payment in payment repo impl");
+                throw;
+            }
+        }
+    }
+}
